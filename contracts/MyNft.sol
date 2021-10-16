5ea22a581d809aa6465d6c997e14662f0d3c1e5d
Asadullah Khan <asadmuhammad427@gmail.com>
2021-10-16 17:36:53 +0500

    commit

--- contracts/MyNft.sol
+++ contracts/MyNft.sol
@@ -1,13 +1,15 @@
+//SPDX-License-Identifier: un-licence
 pragma solidity ^0.8.0;
 
 import "./ERC721Tradeable.sol";
 
-
-
-contract MyNft is ERC721Tradable{
-    constructor() 
-         ERC721Tradable("MyNft","MN","https://opensea-creatures-api.herokuapp.com/api/creature/", 0xF57B2c51dED3A29e6891aba85459d600256Cf317){
-             
-         }
-
-} 
+contract MyNft is ERC721Tradable {
+    constructor()
+        ERC721Tradable(
+            "MyNft",
+            "MN",
+            "https://opensea-creatures-api.herokuapp.com/api/creature/",
+            0xF57B2c51dED3A29e6891aba85459d600256Cf317
+        )
+    {}
+}
